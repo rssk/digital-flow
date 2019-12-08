0e803dabf9860450942c522d8294ea5cf0aa09a1
Ross Kelly <rkelly@mrn.org>
2019-12-08 14:27:38 -0700

    cleanup output

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const { spawn } = require('child_process');
 const wss = new WebSocket.Server({ port: 8789 });
 
 wss.on('connection', (ws) => {
-  const bmon = spawn('bmon', ['-p', 'eth0', '-o', '\'format:fmt=$(attr:rxrate:bytes)\n\'']);
-
+  const bmon = spawn(
+		'bmon', ['-p', 'eth0', '-o', `format:fmt='$(attr:rxrate:bytes)\n'`],
+	);
   bmon.stdout.on('data', (rxData) => {
-    ws.send(rxData);
+    ws.send(rxData.toString().replace(/[']+/g, ''));
+  });
+  bmon.stderr.on('data', (err) => {
+		console.error(`Command error: ${err}`);
   });
 });
