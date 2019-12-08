dab299d214087d369b218f49aaac50bffb674aa7
Ross Kelly <rkelly@mrn.org>
2019-12-08 13:33:30 -0700

    correct byte format

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { spawn } = require('child_process');
 const wss = new WebSocket.Server({ port: 8789 });
 
 wss.on('connection', (ws) => {
-  const bmon = spawn('bmon', ['-p', 'eth0', '-o', 'format:fmt=$(attr:rxrate:bytes)\n']);
+  const bmon = spawn('bmon', ['-p', 'eth0', '-o', '\'format:fmt=$(attr:rxrate:bytes)\n\'']);
 
   bmon.stdout.on('data', (rxData) => {
     ws.send(rxData);
